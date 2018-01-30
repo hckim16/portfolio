1edb1441fde8af7428e2ca09dc93c552f68e6580
Hyo Chang Kim <hck@hyochangkim.com>
2018-01-29 20:45:45 -0500

    add functionality

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    var backgroundImg = [
+        'https://images.unsplash.com/photo-1448523183439-d2ac62aca997?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c6e224d9b386f6715f0ee45295f2211b&auto=format&fit=crop&w=2100&q=80',
+        'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e5a31d03ddee66863a599421f792e07b&auto=format&fit=crop&w=2100&q=80',
+        'https://images.unsplash.com/photo-1488149048941-581936ced6d6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=73ec4df3ea8bdde79a812ac56bb70a01&auto=format&fit=crop&w=1287&q=80',
+        'https://images.unsplash.com/photo-1483058712412-4245e9b90334?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7c5008952226f48ed4bf5d3ea64ff545&auto=format&fit=crop&w=2100&q=80'
+    ];
+
+    $('body').css('background-image', 'url(' + backgroundImg[0] + ')');
+
+
+});
